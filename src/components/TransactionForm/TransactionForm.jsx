f1c4282098f2b8e2a3e7e5ae5632d7a290d77549
AnnaSobchenko <annsbchnk@gmail.com>
2022-02-01 15:43:02 +0200

    first

---
+++ src/components/TransactionForm/TransactionForm.jsx
@@ -0,0 +1,40 @@
+const TransactionForm = () => {
+  return (
+    <>
+      <select name="transType" id="">
+        <option value="incomes">Incomes</option>
+        <option value="costs">Costs</option>
+      </select>
+      <form action="">
+        <label>
+          Day
+          <input type="date" />
+        </label>
+
+        <label>
+          Time
+          <input type="time" />
+        </label>
+
+        <label>
+          Category
+          <input type="button" value="Eats" />
+        </label>
+
+        <label>Sum
+          <input type="text" placeholder="Insert sum" />
+        </label>
+
+        <label>Currency
+          <input type="button" value="UAH" />
+        </label>
+
+        <label>
+          <input type="text" placeholder="Comment"/>
+        </label>
+      </form>
+    </>
+  );
+};
+
+export default TransactionForm;
